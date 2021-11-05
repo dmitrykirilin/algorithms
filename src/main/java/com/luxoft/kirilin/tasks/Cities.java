0efb0bde6b0d7652833fcc6ac250f08855f224a8
d.kirilin <d.kirilin@amg-bs.ru>
2021-11-06 01:56:09 +0300

    Cities routes algorithm. (input - 'cities_input.txt')

--- src/main/java/com/luxoft/kirilin/tasks/Cities.java
+++ src/main/java/com/luxoft/kirilin/tasks/Cities.java
@@ -3,11 +3,37 @@ package main.java.com.luxoft.kirilin.tasks;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
+
+/**
+ * Для перемещений между из города в город он предпочитает использовать машину.
+ * При этом он заправляется только на станциях в городах, но не на станциях по пути.
+ * Поэтому он очень аккуратно выбирает маршруты, чтобы машина не заглохла в дороге.
+ *
+ * А ещё Петя очень важный член команды, поэтому он не может себе позволить путешествовать
+ * слишком долго. Он решил написать программу, которая поможет ему с выбором очередного
+ * путешествия. Но так как сейчас у него слишком много других задач, он попросил вас помочь ему.
+ *
+ * Расстояние между двумя городами считается как сумма модулей разности по каждой из координат.
+ * Дороги есть между всеми парами городов.
+ *
+ * Формат ввода
+ * В первой строке входных данных записано количество городов
+ * n(2 ≤ n ≤ 1000)
+ * В следующих n строках даны два целых числа: координаты каждого города, не превосходящие
+ * по модулю миллиарда. Все города пронумерованы числами от 1 до n в порядке записи
+ * во входных данных. В следующей строке записано целое положительное число k,
+ * не превосходящее двух миллиардов, — максимальное расстояние между городами,
+ * которое Петя может преодолеть без дозаправки машины.
+ * В последней строке записаны два различных числа — номер города, откуда едет Петя,
+ * и номер города, куда он едет.
+ *
+ * Формат вывода
+ * Если существуют пути, удовлетворяющие описанным выше условиям, то выведите минимальное
+ * количество дорог, которое нужно проехать, чтобы попасть из начальной точки маршрута в конечную.
+ * Если пути не существует, выведите -1.
+ */
 
 public class Cities {
 
@@ -17,30 +43,32 @@ public class Cities {
 
     public static void main(String[] args) throws IOException {
         String input = new Scanner(new File(args[0]), Charset.defaultCharset()).useDelimiter("\\Z").next();
-        List<String> rows = new ArrayList<>(Arrays.asList(input.split("\\n")));
+        List<String> rows = new ArrayList<>(Arrays.asList(input.split("\\r\\n")));
 
         int numberOfCities = Integer.parseInt(rows.remove(0));
 
         List<Location> locations = new ArrayList<>();
 
-        for (int i = 1; i <= numberOfCities; i++) {
+        for (int i = 0; i < numberOfCities; i++) {
             String[] coordinates = rows.get(i).split(" ");
             Location location = new Location(Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1]));
             locations.add(location);
         }
+        capacity = Integer.parseInt(rows.get(numberOfCities));
 
         locations.forEach(location -> {
-            location.setAvailableLocations(locations.stream()
+            List<Location> availableLocations = locations.stream()
+                    .filter(loc -> !loc.equals(location))
                     .filter(loc -> resolveDistance(loc, location))
-                    .collect(Collectors.toList()));
+                    .collect(Collectors.toList());
+            location.setAvailableLocations(availableLocations);
         });
 
-        capacity = Integer.parseInt(rows.get(numberOfCities + 1));
-        String[] route = rows.get(numberOfCities + 2).split(" ");
-        Location startLocation = locations.get(Integer.parseInt(route[0]));
-        finishLocation = locations.get(Integer.parseInt(route[1]));
+        String[] route = rows.get(numberOfCities + 1).split(" ");
+        Location startLocation = locations.get(Integer.parseInt(route[0]) - 1);
+        finishLocation = locations.get(Integer.parseInt(route[1]) - 1);
 
-        for (int i = 2; i < numberOfCities; i++) {
+        for (int i = 1; i < numberOfCities; i++) {
             boolean resolved = computeRoute(startLocation, i);
             if(resolved) {
                 System.out.println(i);
@@ -51,7 +79,7 @@ public class Cities {
     }
 
     private static boolean computeRoute(Location location, int i) {
-        if(i == 2) {
+        if(i == 1) {
             return resolveDistance(location, finishLocation);
         }
         i--;
@@ -79,28 +107,26 @@ public class Cities {
             this.y = y;
         }
 
-        public int getX() {
-            return x;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
         public List<Location> getAvailableLocations() {
             return availableLocations;
         }
 
         public void setAvailableLocations(List<Location> availableLocations) {
             this.availableLocations = availableLocations;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Location location = (Location) o;
+            return x == location.x &&
+                    y == location.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 
